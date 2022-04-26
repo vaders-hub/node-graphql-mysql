c50ac06bcbae55681f353e77d9e33b2421e6d523
hyunbae.jeon <mazingazz@gmail.com>
2022-04-26 16:55:10 +0900

    2022.04.26 00

--- app/src/controller/gql/mutation.ts
+++ app/src/controller/gql/mutation.ts
@@ -8,16 +8,19 @@ export default {
     parent: Record<string, unknown>,
     args: Record<string, unknown>
   ) => {
-    const {payload:{member_id, title, body}}: any = args
+    const {
+      payload: { member_id, title, body },
+    }: any = args;
     const queryString = `INSERT INTO mybbs(member_id, title, body)
           VALUES("${member_id}", "${title}", "${body}")`;
 
     try {
       const result = await queryResult(queryString);
-      if (result) return { code: "0000" };
+      if (result) {
+        return { code: "0000" };
+      }
     } catch (e) {
       console.log("error", e);
     }
-    
   },
 };
